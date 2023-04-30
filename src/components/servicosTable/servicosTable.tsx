02186520d1f260a0d225f4f37cff0a390e5c1335
Ruhan <ruhan_350@hotmail.com>
2023-04-30 11:56:08 -0300

    mudança de repositório

--- src/components/servicosTable/servicosTable.tsx
+++ src/components/servicosTable/servicosTable.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import getServicos from '../api/servicos';
+import getServicos from '../components/servicos';
 
 export interface Servico {
   codigoServico: number;
